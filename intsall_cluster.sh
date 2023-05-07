f94b6b198e0437ed0e54131efc524df33beb779a
Roman SHamagin <roman.shamagin@7vpn.com>
2023-05-08 01:12:22 +0300

    add generate inventory

--- intsall_cluster.sh
+++ intsall_cluster.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Проверяем, существует ли директория kubespray/inventory/my_cluster
+if [ ! -d "kubespray/inventory/my_cluster" ]; then
+    cp -r "kubespray/inventory/sample" "kubespray/inventory/my_cluster"
+    echo "Директория kubespray/inventory/my_cluster создана."
+else
+    echo "Директория kubespray/inventory/my_cluster уже существует, копирование не будет выполнено."
+fi
 python3 kubespray/setup.py install
 python3 setup.py
 
@@ -26,3 +33,17 @@ if [[ "$INSTALL_ANSWER" =~ [yY] ]]; then
 else
   echo "Установка HAproxy отменена."
 fi
+
+# Проверяем, существует ли файл ~/.kube/config
+if [ ! -f ~/.kube/config ]; then
+    USER=$(cat hosts.yaml | awk '/master/,/ansible_user/ {if(/ansible_user/) print $2}')
+    SERVER=$(cat hosts.yaml | awk '/master/,/ansible_host/ {if(/ansible_host/) print $2}')
+    mkdir -p ~/.kube || true
+    scp $USER@$SERVER:/etc/kubernetes/admin.conf ~/.kube/config
+    # Заменяем 127.0.0.1 на IP-адрес из переменной $SERVER в файле config
+    sed -i '' "s/127.0.0.1/$SERVER/g" ~/.kube/config
+else
+    echo "Файл config уже существует, код не будет выполнен."
+fi
+
+kubectl get nodes
